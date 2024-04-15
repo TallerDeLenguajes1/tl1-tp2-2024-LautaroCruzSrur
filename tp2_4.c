544f238ab3c4ab18fdddf843b29ef1d92405b7be
Lautaro G Cruz Srur <105937766+LautaroCruzSrur@users.noreply.github.com>
2024-04-15 16:43:37 -0300

    agregando la funcion para las pc

--- tp2_4.c
+++ tp2_4.c
@@ -9,6 +9,9 @@ struct {
     char *tipo_cpu; //valores del arreglo tipos
     } typedef compu;
 
+void listaPC(compu* compus);
+void pcVieja(compu* compus);
+void pcMasterRaise(compu* compus);
 int main(){
     srand(time(NULL));
     char tipos[6][10]={"Intel", "AMD", "Celeron", "Athlon", "Core", "Pentium"};
@@ -22,5 +25,25 @@ int main(){
         int numA = rand() % 6;
         compus[i].tipo_cpu=tipos[numA];
     }
+    listaPC(compus);
     return 0;
 }
+void listaPC(compu* compus){
+    printf("Lista de pc: \n");
+    for (int i = 0; i < 5; i++)
+    {
+        printf("----PC N°%d---\n",i +1);
+        printf("Velocidad : %d \n" ,compus[i].velocidad);
+        printf("Año : %d \n" ,compus[i].anio);
+        printf("Cantidad de nucleos : %d \n" ,compus[i].cantidad);
+        printf("Tipo de Cpu: %s\n", compus[i].tipo_cpu);
+    }
+    
+
+};
+void pcVieja(compu* compus){
+
+};
+void pcMasterRaise(compu* compus){
+
+};
